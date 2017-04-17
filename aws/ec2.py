90f87c586a3da6d2b8ad6afdc0774fa35bb3c207
gauravv1 <gaurav_verma2000@yahoo.com>
2017-04-17 11:08:46 -0400

    Add files via upload

--- aws/ec2.py
+++ aws/ec2.py
@@ -5,55 +5,72 @@ import subprocess
 ec2 = boto3.client('ec2')
 
 def list_ec2():
-	response = ec2.describe_instances()
-	for reservation in response["Reservations"]:
-		for instance in reservation["Instances"]:
-			print("Instance ID is: % s" % instance["InstanceId"])
-		        for i in response["Reservations"]:
-                		for x in i["Instances"]:
-					state = (x["State"])
-					print("Instance State is: % s" % state['Name'])
+        response = ec2.describe_instances()
+        for reservation in response["Reservations"]:
+                for instance in reservation["Instances"]:
+                        print("Instance ID is: % s" % instance["InstanceId"])
+                        for i in response["Reservations"]:
+                                for x in i["Instances"]:
+                                        state = (x["State"])
+                                        print("Instance State is: % s" % state['Name'])
 
 def desc_ec2():
-	instance = raw_input("Enter Instance id for which you want to get details:")
-	response = ec2.describe_instances(InstanceIds=[instance])
-	for i in response["Reservations"]:
-		for x in i["Instances"]:
+        instance = raw_input("Enter Instance id for which you want to get details:")
+        response = ec2.describe_instances(InstanceIds=[instance])
+        for i in response["Reservations"]:
+                for x in i["Instances"]:
 
-			print("Instance ID is: % s" % x["InstanceId"])
-			state = (x["State"])
-			print("Instance State is: % s" % state['Name'])
-			print("Instance Public DNS is: % s" % x["PublicDnsName"])
-			print("Instance Type: % s" % x["InstanceType"])
-			print("Public IP is: % s" % x["PublicIpAddress"])
-			print("Private IP is: % s" % x["PrivateIpAddress"])
+                        print("Instance ID is: % s" % x["InstanceId"])
+                        state = (x["State"])
+                        print("Instance State is: % s" % state['Name'])
+                        print("Instance Public DNS is: % s" % x["PublicDnsName"])
+                        print("Instance Type: % s" % x["InstanceType"])
+                        print("Public IP is: % s" % x["PublicIpAddress"])
+                        print("Private IP is: % s" % x["PrivateIpAddress"])
 
 def maint():
-	instance = raw_input("Enter the instance name which you want to stop or start:")
-	x = ec2.describe_instances(InstanceIds=[instance])
-	for i in x["Reservations"]:
-		for x in i["Instances"]:
-			state = (x["State"])
-			state_status = state['Name']
-			print(state_status)
+        instance = raw_input("Enter the instance name which you want to stop or start:")
+        x = ec2.describe_instances(InstanceIds=[instance])
+        for i in x["Reservations"]:
+                for x in i["Instances"]:
+                        state = (x["State"])
+                        state_status = state['Name']
+                        print(state_status)
 
-			if state_status != 'running' and state_status != 'stopped':
-				print("Instance is either terminated or in pending state")
-			else:
-				if state_status == 'stopped':
-					ans = raw_input("Instance is already stopped. Do you want to start it[Y/N]:").lower()
-					if ans == 'y':
-						ec2.start_instances(InstanceIds=[instance])
-						print("Instance is started now")
-					else:
-						print("Instance is not started")
-				else:
-					ans = raw_input("Instance is running. Are you sure you want to stop it[Y/N]:").lower()
-					if ans =='Y':
-						ec2.stop_instances(InstanceIds=[instance])
-						print("Instance is running")
-					else:
-						print("Instance is started")
+                        if state_status != 'running' and state_status != 'stopped':
+                                print("Instance is either terminated or in pending state")
+                        else:
+                                if state_status == 'stopped':
+                                        ans = raw_input("Instance is already stopped. Do you want to start it[Y/N]:").lower()
+                                        if ans == 'y':
+                                                ec2.start_instances(InstanceIds=[instance])
+                                                print("Instance is started now")
+                                        else:
+                                                print("Instance is not started")
+                                else:
+                                        ans = raw_input("Instance is running. Are you sure you want to stop it[Y/N]:").lower()
+                                        if ans =='y':
+                                                ec2.stop_instances(InstanceIds=[instance])
+                                                print("Instance is stopped")
+                                        else:
+                                                print("Instance is running")
+
+def rename():
+    instance = str(raw_input("Enter the instance id:"))
+    response = ec2.describe_tags(Filters=[{'Name': 'resource-id', 'Values': [instance]}])
+    if response['Tags']:
+        print ("Instance name is %s" % response['Tags'][0]['Value'])
+        ans = raw_input("Do you want to change the name[Y/N]:").lower()
+        if ans == 'y':
+            name = str(raw_input("Enter new name:"))
+#            ec2.delete_tags(Resources=['instance',],Tags=[{'Key': 'Value'}]
+            ec2.create_tags(Resources=[instance,],Tags=[{'Key': 'Name', 'Value': name}])
+            print ("New name is %s" % name)
+        else:
+            print ("Name has not been changed")
+    else:
+        print "Instance has no name"
+
 
 ###########Starting EC2 Menu#################
 
@@ -61,7 +78,8 @@ message='''Welcome to EC2 Menu:
         1. List Instances
         2. Instance Details
         3. Start/Stop Instance
-        4. Exit'''
+        4. Rename Instance
+        5. Exit'''
 print(message)
 
 is_valid = 0
@@ -79,9 +97,10 @@ elif    main_choice == 2:
 elif    main_choice == 3:
         maint()
 elif    main_choice == 4:
-	print 'Good Bye!'        
+        rename()
+elif    main_choice == 5:
+        print 'Good Bye!'
 else:
         print("Please enter correct choice")
 
 ##########End of EC2 menu###############
-
